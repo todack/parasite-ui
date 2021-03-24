03464b513b5667fff6ed0f1882ba320e1158b2b6
Harshal Nandigramwar <pro.bbcom18@gmail.com>
2021-03-24 21:03:49 +0530

    Added inputs in the landing page

--- src/components/landing/Header.vue
+++ src/components/landing/Header.vue
@@ -10,11 +10,25 @@
             An Unified API for Distributed & Multi-Sampled Deep Learning
             Inference
           </v-col>
-          <v-col cols="12" class="d-flex mt-5">
-            <v-btn class="mr-5" width="200" dark large>
-              Sign in with Google
-            </v-btn>
-            <v-btn width="200" dark large> Sign in with GitHub </v-btn>
+          <v-col cols="10" class="mt-5">
+            <v-slide-y-transition hide-on-leave leave-absolute>
+              <p v-if="isAuthenticated" class="title mt-9">
+                Welcome!
+                <span class="font-weight-bold headline">{{ email }}</span
+                >.
+              </p>
+              <v-text-field
+                v-else
+                class="title"
+                @keydown.enter="changeInput"
+                v-model="input"
+                append-icon="mdi-arrow-right"
+                outlined
+                color="grey darken-3"
+                full-width
+                :placeholder="inputPH"
+              ></v-text-field>
+            </v-slide-y-transition>
           </v-col>
         </v-row>
       </v-col>
@@ -22,5 +36,36 @@
         <v-img eager src="@/assets/landingHeader.svg"></v-img>
       </v-col>
     </v-row>
-  </v-container> </template
->>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      input: "",
+      inputPH: "Email Please!",
+      isAuthenticated: false,
+      email: ""
+    };
+  },
+  methods: {
+    changeInput(data) {
+      console.log(data);
+      if (this.inputPH === "Email Please!") {
+        this.email = this.input;
+        this.input = "";
+        this.inputPH = "And password too :)";
+      } else {
+        this.attemptAuthentication({ email: this.email, password: this.input });
+        this.input = "";
+        this.inputPH = "We sent a secret code :D";
+      }
+    },
+    attemptAuthentication(userData) {
+      console.log(userData);
+      this.isAuthenticated = true;
+    }
+  }
+};
+</script>
