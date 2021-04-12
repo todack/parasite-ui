bec5264a85762c8477ce068063c9adab1dff507a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-12 10:32:57 +0000

    Deploy parasite.todack.com to github.com/todack/parasite-ui.git:release

---
+++ precache-manifest.84d33bd9ebf8442b5032bcadc16ac807.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ea2673617fe57343548",
+    "url": "/css/app.f4e0c2aa.css"
+  },
+  {
+    "revision": "109b17580b400fd25620",
+    "url": "/css/chunk-vendors.842e967e.css"
+  },
+  {
+    "revision": "d52b7a44030d12cb0b76d33d5fda5c25",
+    "url": "/img/didntFind.d52b7a44.svg"
+  },
+  {
+    "revision": "2a7a8d38e530a23573fd6ebec7d36630",
+    "url": "/img/distributed.2a7a8d38.svg"
+  },
+  {
+    "revision": "d489386a90e5e256c332056323fc9994",
+    "url": "/img/landingHeader.d489386a.svg"
+  },
+  {
+    "revision": "f76169ba0e2b7b5fa40223f092b7859a",
+    "url": "/img/multiSampled.f76169ba.svg"
+  },
+  {
+    "revision": "f663df7f57bea6e9b5248690265d1421",
+    "url": "/img/support.f663df7f.svg"
+  },
+  {
+    "revision": "082f884f5b06cd951fd0d7256f84cb00",
+    "url": "/img/unifiedInterface.082f884f.svg"
+  },
+  {
+    "revision": "f96bdb1640a29fa310636492fc27afee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ea2673617fe57343548",
+    "url": "/js/app.2965be07.js"
+  },
+  {
+    "revision": "109b17580b400fd25620",
+    "url": "/js/chunk-vendors.b6b188f4.js"
+  },
+  {
+    "revision": "b16d5eba6e0bfab28d1976343015197c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
